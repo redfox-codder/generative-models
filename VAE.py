a83102ac4acc7ba4d7184b096366e8944b7946b9
Ahmad Asgharian Rezaei <35867461+Ahmadreza401@users.noreply.github.com>
2018-02-09 14:17:56 +1100

    Update VAE.py

--- VAE.py
+++ VAE.py
@@ -1,7 +1,5 @@
 from __future__ import print_function, division
 from builtins import range, input
-# Note: you may need to update your version of future
-# sudo pip install -U future
 
 import os
 import scipy as sp
